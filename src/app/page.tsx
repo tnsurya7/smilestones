aa97b10240c119c15f39e85140e9ae8c7b2aebb8
tnsurya7 <suryakumar56394@gmail.com>
2026-03-01 23:26:36 +0530

    Reorder homepage: About intro first, then Mission/Vision side by side

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,8 @@ export default function Home() {
     <main>
       <Header />
       <Hero />
-      <MissionVision />
       <About />
+      <MissionVision />
       <WhyChooseUs />
       <Testimonials />
       <FloatingButtons />
